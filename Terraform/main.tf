a514292af8ca4888183eb343fcc9fe6191c6fe6e
saanvikitjan2024 <saanvikitjan2024@gmail.com>
2024-02-21 11:02:26 +0530

    terraform vm

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -14,7 +14,7 @@ provider "azurerm" {
 
 # Create a azure resource group
 resource "azurerm_resource_group" "rg" {
-  name     = var.resourc_group_name
+  name     = var.resource_group_name
   location = var.location
 }
 
@@ -29,6 +29,102 @@ resource "azurerm_storage_account" "storage" {
   tags = var.tags
 }
 
-output "storage_account_endpoint" {
-  value = azurerm_storage_account.storage.primary_blob_endpoint
+# Create Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  depends_on = [ azurerm_network_security_group.nsg ]
+  name                = var.virtual_network_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = var.virtual_network_address
+  tags                = var.tags
+}
+
+# Create subnet
+resource "azurerm_subnet" "subnet" {
+  depends_on = [ azurerm_virtual_network.vnet, azurerm_network_security_group.nsg]
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.subnet_address
+}
+
+# Create a publicip
+resource "azurerm_public_ip" "publicip" {
+  name                = var.publicip_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+  
+
+  tags = var.tags
+}
+
+# Create a Network security group
+resource "azurerm_network_security_group" "nsg" {
+  name                = var.nsg_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "RDP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = var.tags
+}
+
+# Associate nsg with subnet
+resource "azurerm_subnet_network_security_group_association" "example" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# Create a NIC
+resource "azurerm_network_interface" "nic" {
+  depends_on = [ azurerm_public_ip.publicip, azurerm_virtual_network.vnet, azurerm_subnet.subnet ]
+  name                = var.nic_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.publicip.id
+  }
+}
+
+# Create a windows virtual machine
+resource "azurerm_windows_virtual_machine" "vm" {
+  depends_on = [ azurerm_network_interface.nic ]
+  name                = var.vm_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = var.vm_size
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  #zone                = 3
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  os_disk {
+    name                 = "${var.vm_name}-osdisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
 }
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,4 +1,4 @@
-variable "resourc_group_name" {
+variable "resource_group_name" {
   type        = string
   description = "Name of the resourcegroup"
   default     = "azurerm-terraform-group"
@@ -19,4 +19,58 @@ variable "tags" {
     environment = "prod"
     department  = "IT-Cloud"
   }
+}
+
+variable "virtual_network_name" {
+  type    = string
+  default = "arm-terraform-vnet"
+}
+
+variable "virtual_network_address" {
+  type    = list(any)
+  default = ["192.168.0.0/24"]
+}
+
+variable "subnet_name" {
+  type    = string
+  default = "arm-snet"
+}
+
+variable "subnet_address" {
+  type    = list(any)
+  default = ["192.168.0.0/25"]
+}
+
+variable "publicip_name" {
+  type    = string
+  default = "arm-pip"
+}
+
+variable "nsg_name" {
+  type    = string
+  default = "arm-nsg"
+}
+
+variable "nic_name" {
+  type    = string
+  default = "arm-nic"
+}
+
+variable "vm_name" {
+  type    = string
+  default = "arm-vm"
+}
+
+variable "vm_size" {
+  type    = string
+  default = "Standard_Ds1_v2"
+}
+
+variable "admin_username" {
+  type    = string
+  default = "azureuser"
+}
+
+variable "admin_password" {
+  type    = string
 }
