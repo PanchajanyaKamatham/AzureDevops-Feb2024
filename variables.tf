cc05d11dff207606a5d9399e34968c1b40323fe8
azureramakrishna <83686453+azureramakrishna@users.noreply.github.com>
2024-02-27 10:51:34 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resourcegroup"
+  default     = "azurerm-terraform-group"
+}
+
+variable "location" {
+  type    = string
+  default = "uksouth"
+}
+
+variable "storage_account_name" {
+  type    = string
+  default = "Saanvikit2222"
+}
+
+variable "tags" {
+  default = {
+    environment = "prod"
+    department  = "IT-Cloud"
+  }
+}
+
+variable "count_value" {
+  type    = number
+  default = 20
+}
