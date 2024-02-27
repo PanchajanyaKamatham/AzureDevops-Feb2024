dc4b6cf2f21b8788a00b4fc3854b28118593769d
azureramakrishna <83686453+azureramakrishna@users.noreply.github.com>
2024-02-27 10:53:02 +0530

    Update storage.main.tf

--- storage.main.tf
+++ storage.main.tf
@@ -1,32 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.70.0"
-    }
-  }
-}
-
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-
-  client_id       = var.clientid
-  client_secret   = var.secret
-  tenant_id       = var.tenantid
-  subscription_id = var.subscriptionid
-}
-
-# Azure terraform backend
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "storage-account-resource-group"
-    storage_account_name = "saanvikit20240118"
-    container_name       = "tfstate"
-    key                  = "multistorage.terraform.tfstate"
-  }
-}
-
 # Create a azure resource group
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
@@ -35,7 +6,7 @@ resource "azurerm_resource_group" "rg" {
 
 # Create a storage account
 resource "azurerm_storage_account" "storage" {
-  name                     = 
+  name                     = "${var.storage_account_name}${count.index}"
   count                    = var.count_value
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
